5112dbb64b00763df72e30dc23d9f1b93c611c6f
O.Bai <o.buy.off@gmail.com>
2017-10-02 17:27:58 +0300

    Init Rest

--- client/app/components/navigationBar.js
+++ client/app/components/navigationBar.js
@@ -3,30 +3,30 @@ import {Link} from 'react-router';
 
 export default class NavigationBar extends React.Component{
 
-    render(){
-      const notLogged=(
-        <ul className="nav navbar-nav navbar-right">
-        <li><Link to="/login"><strong>Hi {this.props.username} !</strong></Link></li>    
-        <li><Link to="/signup"><strong>Sign up</strong></Link></li>
-        <li><Link to="/login"><strong>Login</strong></Link></li>
-        </ul>
-      );
+  const notLogged=(
+    <ul className="nav navbar-nav navbar-right">
+    <li><Link to="/login"><strong>Hi {this.props.username} !</strong></Link></li>
+    <li><Link to="/signup"><strong>Sign up</strong></Link></li>
+    <li><Link to="/login"><strong>Login</strong></Link></li>
+    </ul>
+  );
+  const logged = (
+    <div>
+      <ul className="nav navbar-nav navbar-left">
+    <li>
+    <Link to="/profile"><span className="glyphicon glyphicon-user"></span><strong> Hi, {this.props.username}!</strong></Link>
+    </li>
+    </ul>
+    <ul className="nav navbar-nav navbar-right">
 
-      const logged = (
-        <div>
-        <ul className="nav navbar-nav navbar-left">
-        <li>
-        <Link to="/profile"><span className="glyphicon glyphicon-user"></span><strong> Hi, {this.props.username}!</strong></Link>
-        </li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-        
-        <li><Link to="/shoppingcart"><strong><span className="glyphicon glyphicon-shopping-cart"></span> Shopping cart</strong></Link></li>
-        <li><Link to="/shoppinghistory"><strong><span className="glyphicon glyphicon-list"></span> History</strong></Link></li>
-        <li onClick={this.props.logout()}><Link to='/login'><strong>Logout</strong></Link></li>
-     </ul>
-     </div>
-      )
+    <li><Link to="/shoppingcart"><strong><span className="glyphicon glyphicon-shopping-cart"></span> Shopping cart</strong></Link></li>
+    <li><Link to="/shoppinghistory"><strong><span className="glyphicon glyphicon-list"></span> History</strong></Link></li>
+    <li onClick={this.props.logout()}><Link to='/login'><strong>Logout</strong></Link></li>
+ </ul>
+ </div>
+  )
+
+    render(){
 
      return  (
      <nav className="navbar navbar-default bg-inverse " style= {{ backgroundColor : 'rgba(0, 220, 255, 0.5)'}}>
@@ -34,7 +34,7 @@ export default class NavigationBar extends React.Component{
           <div className="navbar-header">
             <Link to="/" className="navbar-brand"><strong>ES Shop</strong></Link>
           </div>
-          
+
           <div className="collapse navbar-collapse">
           { this.props.isAuth ? logged : notLogged }
           </div>
